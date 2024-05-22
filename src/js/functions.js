1f00254b20e15b098c0b873cf2dcda3fb4516fd5
KPetrosyants <kostya.petrosyants@bk.ru>
2024-05-22 20:55:23 +1000

    first commit

---
+++ src/js/functions.js
@@ -0,0 +1,4 @@
+export const foo = 42;
+export const foo2 = function (foo) {
+  return foo + 2;
+};
---
+++ src/js/main.js
@@ -0,0 +1,4 @@
+import "../css/main.css";
+import { foo, foo2 } from "./functions.js";
+console.log(foo);
+console.log(foo2(foo));
