7d26cdd35b9180c635af7dd1d78f0771751b2cef
KPetrosyants <kostya.petrosyants@bk.ru>
2024-06-02 21:33:19 +1000

    swiper fix

---
+++ src/src/js/main.js
@@ -0,0 +1,10 @@
+import "../assets/scss/main.scss";
+
+// components
+import { useTheme } from "./components/theme.js";
+import { useBurger } from "./components/burger.js";
+import { useHeaderHiden } from "./components/header-hiden.js";
+
+useTheme();
+useBurger();
+useHeaderHiden();
